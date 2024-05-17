42eb7f3d8aa9c69d395f85ba699d7ac333dfce1a
MARKTMM <markmwangi304@gmail.com>
2024-05-17 21:18:13 +0300

    Implement basic context load test for Spring Boot application verification

---
+++ src/test/java/com/itsmark/Markhotel/MarkHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsmark.Markhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
