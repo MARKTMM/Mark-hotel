bfe09f98b56e041f0f14f3d4d4dbbe4a40d4fbb5
MARKTMM <markmwangi304@gmail.com>
2024-05-17 20:32:46 +0300

    Create Spring Boot application main class

---
+++ src/main/java/com/itsmark/Markhotel/MarkHotelApplication.java
@@ -0,0 +1,13 @@
+package com.itsmark.Markhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkHotelApplication.class, args);
+	}
+
+}
